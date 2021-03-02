b3f404e730eaa310bda47703fd27a8bcce9d63f8
hmhard <miniye6453@gmail.com>
2021-03-02 16:40:01 +0300

    initial

---
+++ src/Form/UserType.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email',EmailType::class)
+            ->add('roles', ChoiceType::class,[
+                "mapped"=>false,
+                "multiple"=>true,
+                "choices" => ["Select Roles"=>null,"Role Admin" => "ROLE_ADMIN","Role Employee"=>"ROLE_EMPLOYEE","Role Manager"=>"ROLE_MANAGER"]]
+                )
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'The password fields must match.',
+                'options' => ['attr' => ['class' => 'password-field']],
+                'required' => true,
+                'first_options'  => ['label' => 'Password'],
+                'second_options' => ['label' => 'Repeat Password'],
+            ])
+            ->add('firstName')
+            ->add('middleName')
+            ->add('lastName')
+            ->add('gender', ChoiceType::class,["choices" => ["Select Gender"=>null,"Male" => "M","Female"=>"F"]])
+            ->add('dateOfBirth')
+            ->add('isActive')
+            ->add('createdAt')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
